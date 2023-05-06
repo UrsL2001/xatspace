bf459f5702d65901aa10dbe390eab6b2cd844a53
UrsL <131500314+UrsL2001@users.noreply.github.com>
2023-05-06 12:02:37 -0300

    Create seu-arquivo.js

---
+++ seu-arquivo.js
@@ -0,0 +1,6 @@
+var meuBotao = document.getElementById("meu-botao");
+var minhaDiv = document.getElementById("minha-div");
+
+meuBotao.addEventListener("click", function() {
+  minhaDiv.innerHTML = "<iframe src='https://ursl2001.github.io/secret/'></iframe>";
+});
