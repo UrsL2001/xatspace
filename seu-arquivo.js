97702bb37135489b3c9d19c9a5cb123b3e333eed
UrsL <131500314+UrsL2001@users.noreply.github.com>
2023-05-06 12:06:17 -0300

    Delete seu-arquivo.js

--- seu-arquivo.js
+++
@@ -1,6 +0,0 @@
-var meuBotao = document.getElementById("meu-botao");
-var minhaDiv = document.getElementById("minha-div");
-
-meuBotao.addEventListener("click", function() {
-  minhaDiv.innerHTML = "<iframe src='https://ursl2001.github.io/secret/'></iframe>";
-});
